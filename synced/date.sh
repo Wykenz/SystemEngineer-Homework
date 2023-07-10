905b4f7949097fb66f9fcd516feaedfd3866cbb9
Wykenz <wyk73429@gmail.com>
2023-07-10 16:27:21 +0200

    single webserver

---
+++ synced/date.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+containerid=$(docker ps -aqf "name=web-server")
+epoch=$(docker inspect --format='{{.State.StartedAt}}' $containerid | xargs date +%s -d)
+time=$(TZ=UTC-2 date -d @$epoch '+%F %T')
+echo $time > public-html/time.txt
